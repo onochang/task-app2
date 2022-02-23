b056e87b004ac8444a3c73d224aaa92ced5ea505
yasumasa ono <yasumasa.ono0725@gmail.com>
2022-02-24 01:20:40 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
-  <nav>
+  <header class="header">
+    <fa class="fa-bars" :icon="['fas','bars']" />
+    <fa class="fa-sign-out" :icon="['fas','arrow-right-from-bracket']" />
+  </header>
+
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </nav> -->
+
+  <!-- <router-view/> -->
 </template>
+
+<script>
+import 'normalize.css'
+
+export default {
+  name: 'App'
+}
+
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // text-align: center;
+  // color: #2c3e50;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+h1,h2,p {
+  margin: 0;
+}
+
+body {
+  background-color: #8A47CC;
+}
+
+.header{
+  background-color: #562D80;
+  height: 56px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-shadow: 0px 3px 6px rgba($color: #000000, $alpha: 0.16);
+}
+
+.fa-bars,.fa-sign-out{
+  color: #ffffff;
+  font-size: 20px;
 }
 
 nav {
--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas,far)
+
+
+createApp(App).use(store).use(router).component('fa', FontAwesomeIcon ).mount('#app')
